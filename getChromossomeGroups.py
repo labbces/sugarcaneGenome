a704fbdec8694e08087e0f7e18541539fe938cc1
Diego M Riano-Pachon <diriano@gmail.com>
2024-10-21 16:22:00 -0300

    updating group and generating graph representation

--- getChromossomeGroups.py
+++ getChromossomeGroups.py
@@ -9,7 +9,7 @@ groupcount = 1
 # Threshold for overlap percentage (e.g., 50%)
 overlap_threshold = 0.5
 
-with open(fofn, 'r') as f:
+with open(fofn, 'r') as f, open('chrgraph.txt', 'w') as cg:
     for line in f:
         sourmashfile = line.strip()
         listchrs = []
@@ -22,6 +22,8 @@ with open(fofn, 'r') as f:
                 else:
                     line = line.strip().split(',')
                     chromossome = line[2].split('/')[-1].split('.')[0]
+                    chromossome2= line[4].split('/')[-1].split('.')[0]
+                    cg.write(f'{chromossome}.1\t{chromossome2}.1\t{line[0]}\n')
                     listchrs.append(chromossome)
         
         # Check if the group already exists and calculate overlap
@@ -34,13 +36,13 @@ with open(fofn, 'r') as f:
             
             # If overlap percentage is higher than the threshold
             if overlap_percentage >= overlap_threshold:
-                print(f'Overlap found between {sourmashfile} and group {k}')
+                # print(f'Overlap found between {sourmashfile} and group {k}')
                 group_found = True
                 
-                # Keep the larger list
-                if len(set_listchrs) > len(set_v):
-                    groups[k] = listchrs  # Replace the group with the larger list
-                    print(f'Group {k} updated with {sourmashfile}')
+                # Update the group to keep all unique elements (not just the larger list)
+                combined_list = list(set_v.union(set_listchrs))
+                groups[k] = combined_list
+                # print(f'Group {k} updated with merged data from {sourmashfile}')
                 break
         
         # If no significant overlap, create a new group
@@ -53,4 +55,5 @@ with open(fofn, 'r') as f:
 with open('groups.txt', 'w') as g:
     for k, v in groups.items():
         for chr in v:
-            g.write(f'{k}\t{chr}\n')
+            sp="_".join(chr.split('_')[0:2])
+            g.write(f'{k}\t{sp}\t{chr.upper()}.1\n')
